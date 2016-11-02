8311232e7ce682d48ab6ca5ecaf6801e7e6ededf
bcr <bcr@one2team.com>
2016-11-02 13:51:44 +0100

    Update Test for specific situation

--- test/views/Exemple.js
+++ test/views/Exemple.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
 import Coverflow from '../../lib/reactjs-coverflow';
-var Coverflow2 = require('../../lib/reactjs-coverflow');
 
 export default class Exemple extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 1
+    }
+  }
   render() {
     return (
       <div>
         <form>
+          <button onClick={this.setState.bind(this, {page: 1}, null)} type="button">Page 1</button>
+          <button onClick={this.setState.bind(this, {page: 2}, null)} type="button">Page 2</button>
+          <button onClick={this.setState.bind(this, {page: 3}, null)} type="button">Page 3</button>
+          <button onClick={this.setState.bind(this, {page: 4}, null)} type="button">Page 4</button>
           <Coverflow ref="coverflow"
           style={{width: "100vw", height:"500px"}}
-          margin={(this.state && this.state.margin + "px") || undefined}
           startPosition={1}
           enableScroll={true}
-          animationSpeed={3}>
-            <div style={{width: '150px', height: '150px', backgroundColor: 'pink'}} />
-            <div style={{width: '150px', height: '150px', backgroundColor: 'yellow'}} />
-            <div style={{width: '150px', height: '150px', backgroundColor: 'green'}} />
-            <div style={{width: '150px', height: '150px', backgroundColor: 'orange'}} />
-            <div style={{width: '150px', height: '150px', backgroundColor: 'blue'}} />
-            <div style={{width: '150px', height: '150px', backgroundColor: 'purple'}} />
-            <div style={{width: '100px', height: '150px', backgroundColor: '#000000'}} />
-            <div style={{width: '200px', height: '150px', backgroundColor: 'pink'}} />
+          animationSpeed={0.5}>
+            {this.getPage(this.state.page)}
           </Coverflow>
 
           <input type="text" name="margin" onChange={this.handleMarginChange} />
@@ -51,5 +52,50 @@ export default class Exemple extends Component {
   goAt(num, e) {
     e.preventDefault();
     this.refs.coverflow.goAt(4);
+  }
+  getPage(num) {
+    switch (num) {
+      case 1:
+        return [
+          <div key="0" style={{width: '150px', height: '150px', backgroundColor: 'pink'}} />,
+          <div key="1" style={{width: '150px', height: '150px', backgroundColor: 'yellow'}} />,
+          <div key="2" style={{width: '150px', height: '150px', backgroundColor: 'green'}} />,
+          <div key="3" style={{width: '150px', height: '150px', backgroundColor: 'orange'}} />,
+          <div key="4" style={{width: '150px', height: '150px', backgroundColor: 'blue'}} />,
+          <div key="5" style={{width: '150px', height: '150px', backgroundColor: 'purple'}} />,
+          <div key="6" style={{width: '100px', height: '150px', backgroundColor: '#000000'}} />,
+          <div key="7" style={{width: '200px', height: '150px', backgroundColor: 'pink'}} />
+        ];
+        break;
+      case 2:
+        return null;
+        break;
+      case 3:
+        return [
+          <div key="0" style={{width: '150px', height: '150px', backgroundColor: 'pink'}} />,
+          <div key="1" style={{width: '150px', height: '150px', backgroundColor: 'yellow'}} />,
+          <div key="2" style={{width: '150px', height: '150px', backgroundColor: 'green'}} />,
+          <div key="3" style={{width: '150px', height: '150px', backgroundColor: 'orange'}} />,
+          <div key="4" style={{width: '150px', height: '150px', backgroundColor: 'blue'}} />,
+          <div key="5" style={{width: '150px', height: '150px', backgroundColor: 'purple'}} />,
+          <div key="6" style={{width: '100px', height: '150px', backgroundColor: '#000000'}} />,
+          <div key="7" style={{width: '200px', height: '150px', backgroundColor: 'pink'}} />,
+          <div key="8" style={{width: '150px', height: '150px', backgroundColor: 'pink'}} />,
+          <div key="9" style={{width: '150px', height: '150px', backgroundColor: 'yellow'}} />,
+          <div key="10" style={{width: '150px', height: '150px', backgroundColor: 'green'}} />,
+          <div key="11" style={{width: '150px', height: '150px', backgroundColor: 'orange'}} />,
+          <div key="12" style={{width: '150px', height: '150px', backgroundColor: 'blue'}} />,
+          <div key="13" style={{width: '150px', height: '150px', backgroundColor: 'purple'}} />,
+          <div key="14" style={{width: '100px', height: '150px', backgroundColor: '#000000'}} />,
+          <div key="15" style={{width: '200px', height: '150px', backgroundColor: 'pink'}} />
+        ];
+        break;
+      case 4:
+        return [
+          <div key="0" style={{width: '150px', height: '150px', backgroundColor: 'pink'}} />,
+          <div key="1" style={{width: '150px', height: '150px', backgroundColor: 'yellow'}} />
+        ];
+        break;
+    }
   }
 };
