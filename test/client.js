ddbeb5d9e13d0f959940652beac7f6fad37cae68
Bastien Chevallier <cheval_b@etna-alternance.net>
2017-03-09 11:15:42 +0100

    Update client.js

--- test/client.js
+++ test/client.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import Exemple from './views/Exemple';
+import Exemple from './views/Example';
 
 class App extends Component {
   render() {
-    return (<Exemple />);
+    return (<Example />);
   }
 }
 ReactDOM.render(<App/>, document.getElementById('app'));
